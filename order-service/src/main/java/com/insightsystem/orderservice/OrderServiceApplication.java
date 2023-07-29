e75930ceb85babc477f729ba343acb8ff831c0a7
Abhay Chaudhary <chaudhary.abhay@gmail.com>
2023-07-30 01:10:39 +0530

    discovery services configuration complete

--- order-service/src/main/java/com/insightsystem/orderservice/OrderServiceApplication.java
+++ order-service/src/main/java/com/insightsystem/orderservice/OrderServiceApplication.java
@@ -3,6 +3,7 @@ package com.insightsystem.orderservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class OrderServiceApplication {
 
